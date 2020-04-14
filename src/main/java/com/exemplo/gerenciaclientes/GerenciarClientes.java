00f1f45427a22ea390709ad256ca17eb7b6c9783
Willian Costa <williancosta.amaral@gmail.com>
2020-04-14 16:38:37 -0300

    Preparação de ambiente e configuração do Maven

---
+++ src/main/java/com/exemplo/gerenciaclientes/GerenciarClientes.java
@@ -0,0 +1,13 @@
+package com.exemplo.gerenciaclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciarClientes {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciarClientes.class, args);
+	}
+
+}
